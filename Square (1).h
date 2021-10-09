5457d0ebb7378ba0ca36d18ec39a1899fa3bad43
IvanEgorov1 <80792526+IvanEgorov1@users.noreply.github.com>
2021-10-09 18:29:28 +0300

    Add files via upload

---
+++ Square (1).h
@@ -0,0 +1,22 @@
+﻿#pragma once
+class square {
+private:
+    int x; //координата х
+    int y; //координата y 
+    int length; //длина
+public:
+
+    square(); //конструктор 
+
+    void output(); //вывод на экран
+
+    void changeLeng(); //изменение длины
+
+    void changeCoord(); //изменение координат
+
+    void perimeter();// поиск периметра
+
+    void area(); // поиск площади
+
+    void rotation(); // поворот квадрата 
+};
